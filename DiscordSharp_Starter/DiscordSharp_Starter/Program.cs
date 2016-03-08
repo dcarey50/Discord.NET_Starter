e5d6f2425af7fe91601c81fa3156ebf7ea20d55a
Naamloos <ryandejonge@outlook.com>
2016-03-08 19:03:36 +0100

    added created example bot

--- DiscordSharp_Starter/DiscordSharp_Starter/Program.cs
+++ DiscordSharp_Starter/DiscordSharp_Starter/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DiscordSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DiscordSharp_Starter
@@ -10,6 +12,43 @@ namespace DiscordSharp_Starter
     {
         static void Main(string[] args)
         {
+            DiscordClient client = new DiscordClient();
+            client.ClientPrivateInformation.email = "Your Email";
+            client.ClientPrivateInformation.password = "Your password";
+            client.SendLoginRequest();
+            client.Connect(); // Login request, and then connect using the discordclient i just made.
+            client.UpdateCurrentGame("Bot online!"); // This will display at "Playing: "
+
+
+            client.Connected += (sender, e) =>
+            {
+                Console.WriteLine("Connected! User: " + e.user.Username);
+                // If the bot is connected, this message will show.
+            };
+
+
+            client.PrivateMessageReceived += (sender, e) => // Private message is received
+            {
+                if (e.message == "help")
+                {
+                    e.author.SendMessage("This is a private message!"); 
+                    // Because this is a private message, the bot should send a private message back
+                    // A private message does NOT have a channel
+                }
+            };
+
+
+            client.MessageReceived += (sender, e) => // Channel message is received
+            {
+                if(e.message_text == "help"){
+                    e.Channel.SendMessage("This is a public message!");
+                    // Because this is a public message, 
+                    // the bot should send a message to the channel the message was received.
+                }
+            };
+
+
+            Console.ReadKey(); // If the user presses a key, the bot will shut down.
         }
     }
 }
