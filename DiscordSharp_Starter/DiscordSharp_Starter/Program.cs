dcfd9cb68ba1992a2dd64ee12c2beab9c309e548
Naamloos <ryandejonge@outlook.com>
2016-03-09 18:21:02 +0100

    Updated code with LuigiFan's tips!

--- DiscordSharp_Starter/DiscordSharp_Starter/Program.cs
+++ DiscordSharp_Starter/DiscordSharp_Starter/Program.cs
@@ -12,42 +12,46 @@ namespace DiscordSharp_Starter
     {
         static void Main(string[] args)
         {
+            // First of all, a DiscordClient will be created, and the email and password will be defined.
             DiscordClient client = new DiscordClient();
             client.ClientPrivateInformation.email = "Your Email";
             client.ClientPrivateInformation.password = "Your password";
-            client.SendLoginRequest();
-            client.Connect(); // Login request, and then connect using the discordclient i just made.
-            client.UpdateCurrentGame("Bot online!"); // This will display at "Playing: "
 
+            // Then, we are going to set up our events before connecting to discord, to make sure nothing goes wrong.
 
-            client.Connected += (sender, e) =>
+            client.Connected += (sender, e) => // Client is connected to Discord
             {
                 Console.WriteLine("Connected! User: " + e.user.Username);
                 // If the bot is connected, this message will show.
+                // Changes to client, like playing game should be called when the client is connected,
+                // just to make sure nothing goes wrong.
+                client.UpdateCurrentGame("Bot online!\nPress any key to close this window."); // This will display at "Playing: "
             };
 
 
-            client.PrivateMessageReceived += (sender, e) => // Private message is received
+            client.PrivateMessageReceived += (sender, e) => // Private message has been received
             {
                 if (e.message == "help")
                 {
-                    e.author.SendMessage("This is a private message!"); 
+                    e.author.SendMessage("This is a private message!");
                     // Because this is a private message, the bot should send a private message back
                     // A private message does NOT have a channel
                 }
                 if (e.message.StartsWith("join "))
                 {
-                    string joinID = e.message.Substring(24, 16);
-                    client.AcceptInvite(joinID);
+                    string inviteID = e.message.Substring(e.message.LastIndexOf('/'), e.message.Length - e.message.LastIndexOf('/'));
+                    // Thanks to LuigiFan (Developer of DiscordSharp) for this line of code!
+                    client.AcceptInvite(inviteID);
                     e.author.SendMessage("Joined your discord server!");
-                    Console.WriteLine("Got join request from " + joinID);
+                    Console.WriteLine("Got join request from " + inviteID);
                 }
             };
 
 
-            client.MessageReceived += (sender, e) => // Channel message is received
+            client.MessageReceived += (sender, e) => // Channel message has been received
             {
-                if(e.message_text == "help"){
+                if (e.message_text == "help")
+                {
                     e.Channel.SendMessage("This is a public message!");
                     // Because this is a public message, 
                     // the bot should send a message to the channel the message was received.
@@ -55,13 +59,28 @@ namespace DiscordSharp_Starter
             };
 
 
+            // Now, try to connect to Discord.
+            try{ 
+                // Make sure that IF something goes wrong, the user will be notified.
+                // The SendLoginRequest should be called after the events are defined, to prevent issues.
+                client.SendLoginRequest();
+                client.Connect(); // Login request, and then connect using the discordclient i just made.
+            }catch(Exception e){
+                Console.WriteLine("Something went wrong!\n" + e.Message + "\nPress any key to close this window.");
+            }
+
+            // Done! your very own Discord bot is online!
+
+
+            // Now to make sure the console doesnt close:
             Console.ReadKey(); // If the user presses a key, the bot will shut down.
         }
 
-        public int number(string name, int min, int max)
+        public int stringnumber(string name, int min, int max)
         {
             // Bonus code: returns number based of bytes of string.
             // If something goes wrong, (eg: too long int) it return the min value.
+            // This is fun for commands to "rate" a user or anything else, and make sure the same string returns the same number
             try
             {
                 byte[] namebt = Encoding.UTF8.GetBytes(name);
